23351e6c768c055779d12ecd1d779c8d3b413603
khwankamon <khwan_yyg@outlook.com>
2025-02-03 16:34:03 +0700

    initial commit

---
+++ app/Http/Controllers/AuthManager.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthManager extends Controller
+{
+    function login() {}
+
+    function loginPost(Request $request) {}
+
+    function register() {}
+
+    function registerPost(Request $request) {}
+}
